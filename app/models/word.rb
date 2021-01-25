dfd7fa7887161969d62a6883309dd75533a2f8a0
gabriel-demes <Gabrieldemes@nyu.edu>
2021-01-25 18:51:11 -0500

    all associations are completed

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,11 +5,11 @@ class Word < ActiveRecord::Base
     has_many :similar_words
     has_many :synonyms, :through => :similar_words
     has_many :inverse_similar_words, :class_name => "SimilarWord", :foreign_key => "synonym_id"
-    has_many :inverse_synonyms, :through => :inverse_synonyms, :source => :word
+    has_many :inverse_synonyms, :through => :inverse_similar_words, :source => :word
     has_many :opposite_words
     has_many :antonyms, :through => :opposite_words
     has_many :inverse_opposite_words, :class_name => "OppositeWord", :foreign_key => "antonym_id"
-    has_many :inverse_antonyms, :through => :inverse_antonyms, :source => :word
+    has_many :inverse_antonyms, :through => :inverse_opposite_words, :source => :word
 end
 
 
