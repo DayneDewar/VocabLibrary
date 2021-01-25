72ca26d302c2ccffcf4337f06d69e13a9ad080b7
gabriel-demes <Gabrieldemes@nyu.edu>
2021-01-25 17:26:23 -0500

    fixed self referential associations (synonyms and antonyms)

--- app/models/opposite_word.rb
+++ app/models/opposite_word.rb
@@ -1,6 +1,5 @@
 class OppositeWord < ActiveRecord::Base
 
     belongs_to :word
-    belongs_to :antonym, class_name: 'Word', foreign_key: "word_id"
-
+    belongs_to :antonym, :class_name => "Word"
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,9 +2,14 @@ class Word < ActiveRecord::Base
 
     has_many :word_list_relations
     has_many :vocab_lists, through: :word_list_relations
-    has_many :similarwords
-    has_many :synonyms, through: :similar_words
-
+    has_many :similar_words
+    has_many :synonyms, :through => :similar_words
+    has_many :inverse_similar_words, :class_name => "SimilarWord", :foreign_key => "synonym_id"
+    has_many :inverse_synonyms, :through => :inverse_synonyms, :source => :word
+    has_many :opposite_words
+    has_many :antonyms, :through => :opposite_words
+    has_many :inverse_opposite_words, :class_name => "OppositeWord", :foreign_key => "antonym_id"
+    has_many :inverse_antonyms, :through => :inverse_antonyms, :source => :word
 end
 
 
