9b165b3fb1dfb9edb2cdce443aa3ee7e946678e5
Dayne Dewar <daynedewar@yahoo.com>
2021-01-28 09:12:25 -0500

    commit tree c98caaa8559bc273cfa10eeae509b94f4723c566

--- app/dictionary_api.rb
+++ app/dictionary_api.rb
@@ -2,21 +2,35 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 require 'fileutils'
+require 'oxford_dictionary'
 
-class GetRequester
-   
+class WordRequester
+
     def initialize(word)
         @word = word
     end 
 
+    def get_oxford_body
+        client = OxfordDictionary::Client.new(app_id: '51163151', app_key: 'a359822d89657ed4c907db3e4e5030a4')
+        client = OxfordDictionary.new(app_id: '51163151', app_key: 'a359822d89657ed4c907db3e4e5030a4')
+        filters = { lexicalCategory: 'noun', definitions: ''}
+        entry = client.entry(word: "#{@word}", dataset: 'en-gb', params: filters)
+        entry.to_h
+    end
+
+    def oxford_word
+        get_oxford_body[:word]
+    end
+
+    def oxford_definition
+        get_oxford_body[:results]
+    end
+
+    
     def get_response_body
         uri = URI.parse("https://www.dictionaryapi.com/api/v3/references/collegiate/json/#{@word}?key=d2944aa0-c3cf-4476-9e6e-4652d034f6f2")
         response = Net::HTTP.get_response(uri)
         response.body
-    end
-
-    def name
-        json_parse[0]["meta"]["id"]
     end
 
     def find_audio
@@ -44,13 +58,11 @@ class GetRequester
         pid = fork{ exec 'afplay', "./audio_files/#{@word}.wav" }
     end
 
-    def definition
-        json_parse[0]["def"][0]["sseq"][0][0][1]["dt"][0][1]
-    end
-
-    def json_parse
+    def json_parse_audio
         program = JSON.parse(self.get_response_body)
         program
     end
+
+
 
 end
