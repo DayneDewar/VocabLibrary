e54ca247188f279f461a30e15cb0a3991f970875
Dayne Dewar <daynedewar@yahoo.com>
2021-01-28 12:08:28 -0500

    cleaned up Dictionary/Thesaurus

--- app/dictionary_api.rb
+++ app/dictionary_api.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'fileutils'
 require 'oxford_dictionary'
 
-class WordRequester
+class Dictionary
 
     def initialize(word)
         @word = word
@@ -13,19 +13,22 @@ class WordRequester
     def get_oxford_body
         client = OxfordDictionary::Client.new(app_id: '51163151', app_key: 'a359822d89657ed4c907db3e4e5030a4')
         client = OxfordDictionary.new(app_id: '51163151', app_key: 'a359822d89657ed4c907db3e4e5030a4')
-        # filters = { lexicalCategory: 'noun', }
         entry = client.entry(word: "#{@word}", dataset: 'en-gb', params: {})
-        # entry.to_h
     end
 
     def oxford_word
-        get_oxford_body[:word]
+        get_oxford_body.word
     end
 
     def oxford_definition
         get_oxford_body.results.first.lexicalEntries.first.entries.first.senses.first.definitions.first
     end
 
+    def sound
+        File.write "#{@word}.wav", open("https://media.merriam-webster.com/audio/prons/en/us/wav/#{find_audio_type}/#{find_audio}.wav").read
+        FileUtils.mv("#{@word}.wav", "./audio_files/#{@word}.wav")
+        pid = fork{ exec 'afplay', "./audio_files/#{@word}.wav" }
+    end
     
     def get_response_body
         uri = URI.parse("https://www.dictionaryapi.com/api/v3/references/collegiate/json/#{@word}?key=d2944aa0-c3cf-4476-9e6e-4652d034f6f2")
@@ -33,7 +36,10 @@ class WordRequester
         response.body
     end
 
-
+    def json_parse
+        program = JSON.parse(self.get_response_body)
+        program
+    end
 
     def find_audio
         audio = json_parse[0]["hwi"]["prs"][0]["sound"]["audio"]
@@ -52,20 +58,5 @@ class WordRequester
         end
         type
     end
-
-
-    def sound
-        File.write "#{@word}.wav", open("https://media.merriam-webster.com/audio/prons/en/us/wav/#{find_audio_type}/#{find_audio}.wav").read
-        FileUtils.mv("#{@word}.wav", "./audio_files/#{@word}.wav")
-        pid = fork{ exec 'afplay', "./audio_files/#{@word}.wav" }
-    end
-
-
-    def json_parse_audio
-        program = JSON.parse(self.get_response_body)
-        program
-    end
-
-
 
 end
